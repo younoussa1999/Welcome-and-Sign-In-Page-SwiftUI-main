4136a5cca10195e667ae3ca439ad5647294234ee
Younoussa Tiemogo <younoussatiemogo53@gmail.com>
2021-05-30 09:22:09 +0100

    Add files via upload

---
+++ login/ContentView.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  login
+//
+//  Created by Abu Anwar MD Abdullah on 23/4/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+   
+    var body: some View {
+        WelcomeScreenView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct PrimaryButton: View {
+    var title: String
+    var body: some View {
+        Text(title)
+            .font(.title3)
+            .fontWeight(.bold)
+            .foregroundColor(.white)
+            .frame(maxWidth: .infinity)
+            .padding()
+            .background(Color("PrimaryColor"))
+            .cornerRadius(50)
+    }
+}
+
+
+
+
+
+
